6d2d24571b264fef3aed70c3edf8000b0b766d73
unknown <ausbin@PC88629.ihelse.net>
2015-11-13 12:20:30 +0100

    Fixed emacs testing doodle.

--- testing.el
+++ testing.el
@@ -1,17 +1,20 @@
 (defun run-python-tests (root-dir test-subdir buffer-name)
   (interactive)
-  (if (projectile-project-p)
-      (let ((default-directory (projectile-project-root))
-            (buff (get-buffer-create buffer-name)))
-        (display-buffer buff)
-        (with-current-buffer buff
-          (read-only-mode 0))
-        (shell-command (concat "python -m unittest discover " test-subdir) buff)
-        (with-current-buffer buff
-          (compilation-mode)))))
+  (let ((default-directory (file-name-as-directory root-dir))
+        (buff (get-buffer-create buffer-name)))
+    (if (projectile-project-p)
+        (progn (display-buffer buff)
+               (with-current-buffer buff
+                 (read-only-mode 0))
+               (shell-command (concat "c:/python34/python -m unittest discover " test-subdir) buff)
+               (with-current-buffer buff
+                 (compilation-mode))))))
+
+(defun run-bespoke-tests ()
+  (run-python-tests "c:/projects/bespoke"
+                    "test"
+                    "*python-tests*"))
 
 ;; This hook runs the tests after each save, a TDD-esque workflow.
 (add-hook 'after-save-hook
-          (lambda () (run-python-tests "c:/projects/bespoke"
-                                       "test"
-                                       "*python-tests*")))
+          'run-bespoke-tests)
