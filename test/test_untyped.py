123593e1f4654646376bb03586891c685520be98
unknown <ausbin@PC88629.ihelse.net>
2015-11-16 12:23:27 +0100

    Added some tests for the ast-based class.

--- test/test_untyped.py
+++ test/test_untyped.py
@@ -7,7 +7,20 @@ Person = make_untyped_record('name', 'age')
 
 
 class UntypedRecordTests(unittest.TestCase):
+    def test_constructor_takes_ordered_arguments(self):
+        p = Person('Joe', 42)
+        self.assertEqual(p.name, 'Joe')
+        self.assertEqual(p.age, 42)
+
     def test_constructor_takes_keyword_arguments(self):
         p = Person(age=42, name='Joe')
         self.assertEqual(p.name, 'Joe')
         self.assertEqual(p.age, 42)
+
+    def test_property_setting(self):
+        p = Person(age=42, name='Joe')
+        p.age = 2001
+        p.name = 'HAL'
+
+        self.assertEqual(p.name, 'HAL')
+        self.assertEqual(p.age, 2001)
