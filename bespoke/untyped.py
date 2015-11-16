649b0989218e159753a7841b08e0ea5e57bd357e
unknown <ausbin@PC88629.ihelse.net>
2015-11-16 10:39:24 +0100

    Added property setter.

--- bespoke/untyped.py
+++ bespoke/untyped.py
@@ -6,15 +6,15 @@ def _make_args(*fields):
     return [ast.arg(f, None) for f in fields]
 
 
-def _make_assigner(field):
+def _make_assigner(attr, value):
     return ast.Assign(
         targets=[
             ast.Attribute(
                 value=ast.Name(id='self', ctx=ast.Load()),
-                attr='_' + field,
+                attr=attr,
                 ctx=ast.Store())
         ],
-        value=ast.Name(field, ast.Load()))
+        value=ast.Name(value, ast.Load()))
 
 
 def _make_ctor(*fields, name='__init__'):
@@ -28,7 +28,7 @@ def _make_ctor(*fields, name='__init__'):
         kw_defaults=[])
 
     # Assign the paramters to attributes
-    assigners = [_make_assigner(f) for f in fields]
+    assigners = [_make_assigner('_' + f, f) for f in fields]
 
     func = ast.FunctionDef(
         name=name,
@@ -40,7 +40,7 @@ def _make_ctor(*fields, name='__init__'):
     return func
 
 
-def _make_property(name):
+def _make_property_getter(name):
     args = ast.arguments(
         args=_make_args('self'),
         vararg=None,
@@ -69,13 +69,42 @@ def _make_property(name):
     return func
 
 
+def _make_property_setter(name):
+    args = ast.arguments(
+        args=_make_args('self', 'value'),
+        vararg=None,
+        kwonlyargs=[],
+        kwarg=None,
+        defaults=[],
+        kw_defaults=[])
+
+    body = [_make_assigner('_' + name, 'value')]
+
+    decorators = [
+        ast.Attribute(
+            value=ast.Name(id=name, ctx=ast.Load()),
+            attr='setter',
+            ctx=ast.Load())]
+
+    func = ast.FunctionDef(
+        name=name,
+        args=args,
+        body=body,
+        decorator_list=decorators,
+        returns=None)
+
+    return func
+
+
 def make_untyped_record(*fields):
     class Shell:
         pass
 
-    body = [_make_ctor(*fields)] + [_make_property(f) for f in fields]
+    body = chain([_make_ctor(*fields)],
+                 [_make_property_getter(f) for f in fields],
+                 [_make_property_setter(f) for f in fields])
 
-    mod = ast.Module(body=body)
+    mod = ast.Module(body=list(body))
     mod = ast.fix_missing_locations(mod)
     namespace = {}
     exec(compile(mod, '<ast>', 'exec'), namespace)
